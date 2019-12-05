167b3ef78088eabb4c662a75d0bb0781578d300c
Exofile <myexofile@outlook.com>
2019-12-05 15:03:16 +0100

    Update myfirst.js

--- js/myfirst.js
+++ js/myfirst.js
@@ -1,4 +1,12 @@
 var http = require("http");
+var url = require("url");
+var adr = window.location.href;
+var q = url.parse(adr, true);
+
+console.log(adr);
+
+var qdata = q.query;
+console.log(qdata.code);
 
 http.createServer(function(req, res) {
 	res.writeHead(200, {"Content-Type": "text/html"});
